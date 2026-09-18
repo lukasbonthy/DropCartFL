5f74b11eff70d8e1951f36f8092e0b6e4b50f827
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2026-09-18 13:39:32 -0400

    Constrain skip-link removal to one anchor

--- render-overlay/patch-smooth-flow.mjs
+++ render-overlay/patch-smooth-flow.mjs
@@ -61,7 +61,7 @@ writeFileSync(layoutPath, layout);
 
 const homePath = join(appDir, "page.tsx");
 let home = readFileSync(homePath, "utf8");
-const skipBookingPattern = /\s*<a\b[^>]*>[\s\S]*?Skip to booking[\s\S]*?<\/a>\s*/i;
+const skipBookingPattern = /\s*<a\b(?:(?!<\/a>)[\s\S])*?Skip to booking(?:(?!<\/a>)[\s\S])*?<\/a>\s*/i;
 if (skipBookingPattern.test(home)) {
   home = home.replace(skipBookingPattern, "\n");
   writeFileSync(homePath, home);
